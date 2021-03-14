8354c309715306ab79230f56268ea689c17ad1e0
baxodirovdev <76782350+baxodirovdev@users.noreply.github.com>
2021-03-14 15:58:38 +0500

    fix: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (array && array.length !== 0) {
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length !== 0) {
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length !== 0) {
+        let avg = 0;
+        for (let i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+        let res = avg / array.length;
+        return res;
+    } else {
+        return 0;
+    }
+};
